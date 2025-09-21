e90d05beb1c81660e13715676add4fddfa2183ec
gingie-code <tywilsondawkins@gmail.com>
2025-09-20 23:08:40 -0700

    Refactor: move word count to stats.py and import in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import get_num_words
 def get_book_text(filepath):
     with open(filepath) as f:
         # f is a file object
@@ -6,12 +7,9 @@ def get_book_text(filepath):
 
 # this is a read file example in practice
 # the file object is named f
-def count_words(book_text):
-    words = book_text.split()
-    num_words = len(words)
-    return num_words
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    num_words = count_words(book_text) 
+    num_words = get_num_words(book_text) 
     print(f"{num_words} words found in the document")
 main()
---
+++ stats.py
@@ -0,0 +1,5 @@
+# python
+def get_num_words(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return num_words
