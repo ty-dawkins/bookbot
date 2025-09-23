8ff50eda7acf36ed6b1c4469043175b4f47554cb
gingie-code <tywilsondawkins@gmail.com>
2025-09-22 19:16:59 -0700

    sys arguements and finish stage of a working bookbot

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from stats import get_num_words
 from stats import get_num_characters
+from stats import convert_chars_to_sorted_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+# The rest of your script for processing the book path would go here
+# For example, to print the path:
+# book_path = sys.argv[1]
+# print(f"Processing book: {book_path}")
+
 def get_book_text(filepath):
     with open(filepath) as f:
         # f is a file object
@@ -10,9 +22,19 @@ def get_book_text(filepath):
 # the file object is named f
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(sys.argv[1])
     num_words = get_num_words(book_text) 
     char_counts = get_num_characters(book_text)
-    print(f"{num_words} words found in the document")
-    print(char_counts)
+    sorted_char_list = convert_chars_to_sorted_list(char_counts)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count -----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_char_list:
+        ch = item["char"]
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,22 @@ def get_num_characters(book_text):
         else:
             char_counts[lower_char] = 1
     return char_counts
+#sort helper function
+def sort_on(char_counts):
+    return char_counts["num"]
+    
+letters = [
+    {"char": "a", "num": 25894},
+    {"char": "b", "num": 4868},
+    {"char": "c", "num": 9011},
+    ]
+letters.sort(reverse=True, key=sort_on)
+print(letters)
+
+#A NEW main function- this converts the character dictionary to a list and uses sort_on to sort it
+def convert_chars_to_sorted_list(char_counts):
+    char_counts_list = []
+    for char, num in char_counts.items():
+        char_counts_list.append({"char": char, "num": num})
+    char_counts_list.sort(reverse=True, key=sort_on)
+    return char_counts_list
